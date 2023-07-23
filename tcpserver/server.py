4fbe07497832966e31d21dab9cbc5d33ce6a2674
SaveGamers-Developer-Networks <contact@savegamers.de>
2023-07-24 00:46:34 +0200

    Modifyed server.py
    Bind-IP changed to 127.0.0.1 because of testing

--- tcpserver/server.py
+++ tcpserver/server.py
@@ -81,7 +81,7 @@ class MineRouterServer:
 
 
 def main():
-    with MineRouterServer(host="0.0.0.0", port=25565) as mc:
+    with MineRouterServer(host="127.0.0.1", port=25565) as mc:
         mc.serve_forever()
 
 
